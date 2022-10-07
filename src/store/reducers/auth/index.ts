de106a0f529604df17fa204d975a19d2038e9acd
Your Name <you@example.com>
2022-10-07 11:43:47 +0300

    test1

--- src/store/reducers/auth/index.ts
+++ src/store/reducers/auth/index.ts
@@ -2,7 +2,7 @@ import { IUser } from '../../../models/IUser'
 import { AuthAction, AuthActionsEnum, AuthState } from './types'
 
 const initialState: AuthState = {
-  isAuth: false,
+  isAuth: true,
   error: '',
   isLoading: false,
   user: {} as IUser,
